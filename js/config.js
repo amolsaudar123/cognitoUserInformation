12572861f371443d7975dc40ed6db7975d18a7b7
Amol <amol.saudar@guest-tek.com>
2019-12-31 17:56:47 +0530

    -----Initial Login user information completed

---
+++ js/config.js
@@ -0,0 +1,8 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_lQiRuk3O0', // e.g. us-east-2_uXboG5pAb
+        region: 'ap-south-1', // e.g. us-east-2
+		clientId: '1jg32089gkl6a0nqinrvv9j6so' //is this used anywhere?
+    },
+};
+
